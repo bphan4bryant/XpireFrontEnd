6e7dfa6b100e7ff20e7e731960d412ef891c75f4
tanner9427 <tanner9427@gmail.com>
2024-01-13 03:13:58 -0800

    login background and container implementation

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import './Login.css'
+
+function Login() {
+  return (
+    <div className='Login-background'>
+    <div className="Login-box-container">
+    <form className="Login-box">
+        ah fuck
+    </form>
+    </div>
+    </div>
+  )
+}
+
+export default Login
