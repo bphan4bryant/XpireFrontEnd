2ed91669c117e6bc7ea0ed7421382be7c110441e
tanner9427 <tanner9427@gmail.com>
2024-01-13 15:41:32 -0800

    Login page forgot password and sign up implementations

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,13 +8,14 @@ function Login() {
             
             <form className="Login-box">
                 <img className="Login-image" src="./Xpire.png" alt="logo"/>
-                <h3 className="Login-title">Sign In</h3>
+                {/* <h3 className="Login-title">Sign In</h3> */}
                 <Form>
                     <div className="Login-credentials-title">
                         <Form.Label htmlFor="inputUsername5">Username</Form.Label>
                     </div>
                     <div>
                         <Form.Control
+                            className="Login-entry-box"
                             type="username"
                             id="inputUsername5"
                             aria-describedby="usernameHelpBlock"
@@ -28,8 +29,9 @@ function Login() {
                     <div className="Login-credentials-title">
                         <Form.Label htmlFor="inputPassword5">Password</Form.Label>
                     </div>
-                    <div>
+                    <div >
                         <Form.Control
+                        className="Login-entry-box"
                         width={"100%"}
                             type="password"
                             id="inputPassword5"
@@ -41,11 +43,17 @@ function Login() {
                             }}
                         />
                     </div>
-                    <div className="Login-submit">
-                        <button type="submit" className="btn btn-primary">
-                        Login
-                    </button>
+                    <div className="Login-forgot">
+                         <a className="Login-forgot-link" href="#">Forgot Password?</a>
                     </div>
+                    <div className="Login-submit">
+                        <button type="submit" className="Login-submit-button">
+                        Login
+                        </button>
+                    </div>
+                    <p className="Login-sign-up">
+                        Don't have an account? <a className="Login-sign-up-link" href="#">Sign Up!</a>
+                    </p>
                 </Form>
             </form>
         </div>
