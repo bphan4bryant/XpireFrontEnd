90d35c35956c2b916550b172a92ac15ab26226b2
Dylan Vu <dylanvu9@gmail.com>
2024-01-13 15:46:49 -0800

    fix sizing for mobile

--- src/components/profilePicture.tsx
+++ src/components/profilePicture.tsx
@@ -1,7 +1,11 @@
 import { Col, Image } from "react-bootstrap";
 import "./profilePicture.css"
 
-export default function ProfilePicture({ img }: { img: string }) {
+export default function ProfilePicture({ img }: { img?: string }) {
+    const placeholder = "./placeholder-image.jpg";
+    if (img === undefined || img.length === 0) {
+        img = placeholder
+    }
     return (
         <Image src={img} className="rounded-pfp" alt="profile picture" />
     )
