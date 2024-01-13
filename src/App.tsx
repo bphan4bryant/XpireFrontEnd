cb46180680c5fd23317a19db86f170c80a59ef68
tanner9427 <tanner9427@gmail.com>
2024-01-13 13:10:21 -0800

    login page submit and image implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Login></Login>
     </>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,27 +7,44 @@ function Login() {
         <div className="Login-box-container">
             
             <form className="Login-box">
+                <img className="Login-image" src="./Xpire.png" alt="logo"/>
                 <h3 className="Login-title">Sign In</h3>
                 <Form>
                     <div className="Login-credentials-title">
                         <Form.Label htmlFor="inputUsername5">Username</Form.Label>
                     </div>
-                    <div className="Login-credentials-title">
+                    <div>
                         <Form.Control
                             type="username"
                             id="inputUsername5"
                             aria-describedby="usernameHelpBlock"
+                            style={{
+                                width: "70%",
+                                marginRight: "60px",
+                                marginLeft: "60px"
+                            }}
                         />
                     </div>
                     <div className="Login-credentials-title">
                         <Form.Label htmlFor="inputPassword5">Password</Form.Label>
                     </div>
-                    <div className="Login-credentials-title">
+                    <div>
                         <Form.Control
+                        width={"100%"}
                             type="password"
                             id="inputPassword5"
                             aria-describedby="passwordHelpBlock"
+                        style={{
+                                width: "70%",
+                                marginRight: "60px",
+                                marginLeft: "60px"
+                            }}
                         />
+                    </div>
+                    <div className="Login-submit">
+                        <button type="submit" className="btn btn-primary">
+                        Login
+                    </button>
                     </div>
                 </Form>
             </form>
