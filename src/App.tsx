4f0c59596bfe80ed28b12df64773f018dd650bf4
tanner9427 <tanner9427@gmail.com>
2024-01-13 13:13:36 -0800

    merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Inventory from './pages/Inventory'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import CommonNavbar from './components/CommonNavbar'
+import Login from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <CommonNavbar/>
-      <Inventory/>
+      <Login></Login>
     </>
   )
 }
