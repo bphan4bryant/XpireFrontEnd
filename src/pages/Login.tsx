86940b81162629a10651f0a73b7d36d841051374
tanner9427 <tanner9427@gmail.com>
2024-01-13 11:49:57 -0800

    username and password implementation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,37 @@
 import './Login.css'
+import Form from 'react-bootstrap/Form';
 
 function Login() {
   return (
     <div className='Login-background'>
-    <div className="Login-box-container">
-    <form className="Login-box">
-        ah fuck
-    </form>
-    </div>
+        <div className="Login-box-container">
+            
+            <form className="Login-box">
+                <h3 className="Login-title">Sign In</h3>
+                <Form>
+                    <div className="Login-credentials-title">
+                        <Form.Label htmlFor="inputUsername5">Username</Form.Label>
+                    </div>
+                    <div className="Login-credentials-title">
+                        <Form.Control
+                            type="username"
+                            id="inputUsername5"
+                            aria-describedby="usernameHelpBlock"
+                        />
+                    </div>
+                    <div className="Login-credentials-title">
+                        <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+                    </div>
+                    <div className="Login-credentials-title">
+                        <Form.Control
+                            type="password"
+                            id="inputPassword5"
+                            aria-describedby="passwordHelpBlock"
+                        />
+                    </div>
+                </Form>
+            </form>
+        </div>
     </div>
   )
 }
