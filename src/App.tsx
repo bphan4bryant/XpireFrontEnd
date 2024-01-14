61a32e7142f222a56dc6af6125a8eaa932c56683
Thao Nguyen <tnguyen7263@gmail.com>
2024-01-13 19:09:56 -0800

    leaderboard & logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Inventory from './pages/Inventory'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import CommonNavbar from './components/CommonNavbar'
+import Leaderboard from './pages/Leaderboard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <CommonNavbar/>
-      <Inventory/>
+      <Leaderboard/>
     </>
   )
 }
