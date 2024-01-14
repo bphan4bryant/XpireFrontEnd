16d6c45467a35f72f4dd8a359f8e4464a6898257
Dylan Vu <dylanvu9@gmail.com>
2024-01-14 01:20:04 -0800

    fix navbar link refs

--- src/components/CommonNavbar.tsx
+++ src/components/CommonNavbar.tsx
@@ -9,22 +9,22 @@ function CommonNavbar() {
 
     return (
         <>
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} alt="Xpire" width="90em"/>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt="Xpire" width="90em" />
                     </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/leaderboard" className="fs-5">Leaderboard</Nav.Link>
-                    <Nav.Link href="/Inventory" className="fs-5">Inventory</Nav.Link>
-                    <Nav.Link href="/profile" className="fs-5">Profile</Nav.Link>
-                    <Nav.Link href="/forgot" className="fs-5">Logout</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/leaderboard" className="fs-5">Leaderboard</Nav.Link>
+                            <Nav.Link href="/Inventory" className="fs-5">Inventory</Nav.Link>
+                            <Nav.Link href="/profile" className="fs-5">Profile</Nav.Link>
+                            <Nav.Link href="/" className="fs-5">Logout</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </>
     )
 }
