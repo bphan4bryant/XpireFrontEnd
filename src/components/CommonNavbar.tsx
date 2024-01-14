77e75f93b81531e467e4ecdce7815a00deb60eb5
Thao Nguyen <tnguyen7263@gmail.com>
2024-01-13 19:09:56 -0800

    leaderboard & logo

--- src/components/CommonNavbar.tsx
+++ src/components/CommonNavbar.tsx
@@ -2,14 +2,18 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from './IMG_4609-2.png'
 
+//
 function CommonNavbar() {
 
     return (
         <>
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Xpire Logo</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <div><img src={logo} alt="logo" width="150em"/></div>
+                    </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
---
+++ src/pages/Leaderboard.tsx
@@ -0,0 +1,48 @@
+import Table from 'react-bootstrap/Table'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import {User} from '../types/types'
+import './Leaderboard.css'
+
+function Leaderboard() {
+  const [Leaderboard, setLeaderboard] = useState<User[]>([])
+
+  const client = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL //Change default user to actual user field
+  });
+
+  const getLeaderboard = async() => {
+    const url =  '/leaderboard' 
+    await client.get(url)
+        .then((res) => {console.log(res.data); setLeaderboard(res.data.data)})
+        .catch((err) => console.log(err))
+    }
+
+    useEffect(() => {
+      getLeaderboard()}, [])
+
+  return (
+        <Table hover borderless className="tb">
+          <thead className='thead-dark'>
+            <tr >
+              <th scope="col">Rank</th>
+              <th scope="col">Profile</th>
+              <th scope="col">Name</th>
+              <th scope="col">Points</th>
+            </tr>
+          </thead>
+        
+          <tbody>
+            {Leaderboard.map((item, i) =>
+                <tr key={item.id}>
+                  <th scope="col">{i+1}</th>
+                  <td>{ item.pfp }</td>
+                  <td>{ item.name }</td>
+                  <td>{ item.points }</td>
+                </tr>
+            )}
+          </tbody>
+        </Table>
+  );  
+}
+export default Leaderboard
