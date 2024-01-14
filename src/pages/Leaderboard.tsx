75d3b1b5904dbf335197cbf510b155c7dc109f5a
Thao Nguyen <tnguyen7263@gmail.com>
2024-01-14 01:30:52 -0800

    fix styling

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import {User} from '../types/types'
 import './Leaderboard.css'
 import CommonNavbar from '../components/CommonNavbar'
+import {Container, Row} from 'react-bootstrap'
 
 function Leaderboard() {
   const [Leaderboard, setLeaderboard] = useState<User[]>([])
@@ -25,27 +26,33 @@ function Leaderboard() {
   return (
     <>
       <CommonNavbar/>
-      <Table hover borderless className="tb">
-        <thead className='thead-dark'>
-          <tr >
-            <th scope="col">Rank</th>
-            <th scope="col">Profile</th>
-            <th scope="col">Name</th>
-            <th scope="col">Points</th>
-          </tr>
-        </thead>
-      
-        <tbody>
-          {Leaderboard.map((item, i) =>
-              <tr key={item.id}>
-                <th scope="col">{i+1}</th>
-                <td><img src= {item.pfp} alt='none' width="40em"/></td>
-                <td>{ item.name }</td>
-                <td>{ item.points }</td>
-              </tr>
-          )}
-        </tbody>
-      </Table>
+      <div className='Leaderboard-background'>
+        <Container className='custom-contain'>
+          <Row className='custom-row'>
+            <Table hover size="sm" className='tb'>
+              <thead className='thead-dark'>
+                <tr >
+                  <th scope="col">Rank</th>
+                  <th scope="col">Profile</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Points</th>
+                </tr>
+              </thead>
+            
+              <tbody>
+                {Leaderboard.map((item, i) =>
+                    <tr key={item.id}>
+                      <th scope="col">{i+1}</th>
+                      <td><img src= 'placeholder-image.jpg'width="40em"/></td>
+                      <td>{ item.name }</td>
+                      <td>{ item.points }</td>
+                    </tr>
+                )}
+              </tbody>
+            </Table>
+          </Row>
+        </Container>
+      </div>
     </>
   );  
 }
