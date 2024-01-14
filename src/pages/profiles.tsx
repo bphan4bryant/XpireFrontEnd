9499b8f0e875270197594a69d61d8c748d22a554
Dylan Vu <dylanvu9@gmail.com>
2024-01-14 02:41:37 -0800

    add icons as props

--- src/pages/profiles.tsx
+++ src/pages/profiles.tsx
@@ -3,48 +3,46 @@ import './Login.css'
 import ProfilePicture from '../components/profilePicture'
 import CommonNavbar from '../components/CommonNavbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCarrot } from '@fortawesome/free-solid-svg-icons'
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
-import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
+import { IconDefinition, faCarrot, faTrashCan, faBasketShopping } from '@fortawesome/free-solid-svg-icons'
 
-function Statistic(props: {title: string, number: number}) {
-    return(
+function Statistic(props: { title: string, number: number, icon: IconDefinition }) {
+    return (
         <div>
             <h3>{props.title}</h3>
-            <FontAwesomeIcon icon={faBasketShopping} />
+            <FontAwesomeIcon icon={props.icon} />
             <div>
-            {props.number}
+                {props.number}
             </div>
         </div>
-        )
+    )
 }
 
 function Profile() {
-    return(
+    return (
         <div>
-            <CommonNavbar/>
-                <div className="profile-box-container">
-                    {/* navbar */}
+            <CommonNavbar />
+            <div className="profile-box-container">
+                {/* navbar */}
+                <div>
+                    <h3 className="profile-title">Profile</h3>
                     <div>
-                        <h3 className="profile-title">Profile</h3>
-                        <div>
-                            {/* profile picture */}
-                            <ProfilePicture img="./placeholder-image.jpg"/>
-                        </div>
-                        {/* name */}
-                        <h3 className="profile-info">Name</h3>
-                        {/* email */}
-                        <h3 className="profile-info">Email</h3>
-                        {/* points */}
-                        <h3 className="profile-info">Points: 10</h3>
+                        {/* profile picture */}
+                        <ProfilePicture img="./placeholder-image.jpg" />
+                    </div>
+                    {/* name */}
+                    <h3 className="profile-info">Name</h3>
+                    {/* email */}
+                    <h3 className="profile-info">Email</h3>
+                    {/* points */}
+                    <h3 className="profile-info">Points: 10</h3>
 
-                        <div className="profile-grid">
-                            <Statistic title="Bought" number={5}/>
-                            <Statistic title="Cooked" number={6}/>
-                            <Statistic title="Trashed" number={7}/>
-                        </div>
+                    <div className="profile-grid">
+                        <Statistic title="Bought" number={5} icon={faBasketShopping} />
+                        <Statistic title="Cooked" number={6} icon={faCarrot} />
+                        <Statistic title="Trashed" number={7} icon={faTrashCan} />
                     </div>
                 </div>
+            </div>
         </div>
     )
 }
